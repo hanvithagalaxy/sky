3488e4f13fabe3d34454e595c8ada79465e899b4
harshitha <harshitha@alclabs.in>
2022-11-08 09:58:53 +0530

    this is rim directory

---
+++ prg4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "your username is "
+logname
+echo "your userid is"
+id -u
+echo "no of disks in your server"
+ls /dev/sd?
+#end of program
---
+++ prg5.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "enter the value for x:"
+read x
+echo "the value of the x is:$x"
+#End the program
